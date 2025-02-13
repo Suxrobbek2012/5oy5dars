b1c7967b625d1db2931fad1307eddbb19ace9557
Suxrobbek2012 <suxrobbek714@gmail.com>
2025-02-13 11:22:28 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+// let ism = prompt("ismingizni kriting");
+// const resilt= ism.length
+// alert(`${ism.toUpperCase()} iami ${result} harifdan iborat`)
+
+
+
+// const oquvchi = {
+//     ism:"suxrob",
+//     familya:"baxtiyorov",
+//     yosh:12,
+//     coin:1275,
+//     group:1746,
+//     shahar:"toshkent",
+// }
+
+// console.log(oquvchi.group);
+
+const Name =prompt("ismingizni kriting");
+const userName =prompt("familya kriting");
+const userage =prompt("yosh kriting");
+const hp =prompt("hp kriting");
+const coin =prompt("coin kriting");
+const teacher =prompt("sunnat kriting");
+const group =prompt("gruppa kriting");
+const login =prompt("login kriting");
+const parol =prompt("parol kriting");
+
+
+
+
+
+
+
+
+const oquvchi = {
+    ism:Name,
+familya:userName,
+yosh:userage,
+hp:hp,
+coin:coin,
+teacher:teacher,
+group:group,
+login:login,
+parol:parol,
+}
+console.log(oquvchi);
